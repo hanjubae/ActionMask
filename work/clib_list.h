c6eafd20168365a524fce44560ec4e4d26505345
hanjubae <faithbae41@gmail.com>
2018-07-31 16:52:29 +0900

    move file to work

--- work/clib_list.h
+++ work/clib_list.h
@@ -1,14 +1,14 @@
 #ifndef __D_LINKED_LIST_H__
+
 #define __D_LINKED_LIST_H__
+#define TRUE    1
+#define FALSE   0
 
-#define TRUE	1
-#define FALSE	0
-
-typedef struct clib_info {	
-	char[32] name;
-	char[32] header;
-	char[160] prototype;
-	char[200] description;
+typedef struct clib_info {
+	char name[32];
+	char header[32];
+	char prototype[160];
+	char description[200];
 }Info;
 
 typedef struct clib_node {
@@ -17,14 +17,14 @@ typedef struct clib_node {
 }Node;
 
 typedef struct clib_list {
-	Node head;
+	Node* head;
 	int size;
 }List;
 
 void init(List* plist);
-void push(List* plist, Info data);
-void remove(List* plist, char remove_name[]);
-Node search(List* plist, char find_name[]);
+void push(List* plist, char name[], char header[], char);
+int remove(List* plist, char remove_name[]);
+Info search(List* plist, char find_name[]);
 int size(List* plist);
-#endif
 
+#endif
